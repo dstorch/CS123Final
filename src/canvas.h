067cb37ec98b374c28f6b32225039b04106c1942
David Storch <dstorch@cs.brown.edu>
2011-11-30 20:20:32 -0500

    cleaned up code we don't need

--- src/canvas.h
+++ src/canvas.h
@@ -21,49 +21,9 @@ protected:
 
     // Helper methods
 
-    /**
-      * Converts a grid coordinate (row, column) to an index into a 1-dimensional array.
-      * Can be used to index into m_terrain or m_normalMap.
-      * Returns -1 if the grid coordinate entered is not valid.
-      */
-    inline int getIndex(const GridPoint &c);
-    /**
-      * Converts a grid coordinate (row, column) to an index into a 1-dimensional array.
-      * Can be used to index into m_terrain or m_normalMap.
-      * Returns -1 if the grid coordinate entered is not valid.
-      */
-    inline int getIndex(int row, int column);
-
-    /**
-     * Computes the amount to perturb the height of the vertex currently being processed.
-     * Feel free to modify this.
-     *
-     * @param depth The current recursion depth
-     */
-    double getPerturb(int cur_depth);
-    /**
-     * Retrieves the position of each neighbor of the given grid coordinate (i.e. all grid
-     * coordinates that can be found one unit horizontally, vertically, or diagonally from
-     * the specified grid coordinate).
-     *
-     * @param coordinate The grid coordinate whose neighbors are to be retrieved
-     */
-    QList<Vector3*> getSurroundingVertices(const Vector2 &coordinate);
 
     // Methods you will have to fill in
 
-    /**
-     * You need to fill this in.
-     *
-     * Subdivides a square by finding the vertices at its corners, the midpoints of each side, and
-     * the center (as the algorithm describes). Then recurs on each of the four sub-squares created.
-     *
-     * @param topLeft The grid coordinate of the top-left corner of the square to subdivide
-     * @param bottomRight The grid coordinate of the bottom-right corner of the square to subdivide
-     * @param depth The current recursion depth, decreasing as this function recurses deeper. The
-     *              function should stop recurring when this value reaches zero.
-     */
-    void subdivideSquare(Vector2 tlg, Vector2 brg, GLint curDepth);
     /**
      * You need to fill this in.
      *
@@ -84,11 +44,6 @@ protected:
      * and modelview matrices to match the values in m_camera.
      */
     void updateCamera();
-    /**
-     * Generates a new OpenGL texture and loads the image data from the file at the given path into
-     * the texture. Makes the new texture active and returns its unique texture ID.
-     */
-    GLuint loadTexture(const QString &filename);
 
     /** Sets the initial values of OpenGL state variables used by this lab */
     void initializeGL();
