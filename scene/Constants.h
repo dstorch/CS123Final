e846dfcb732e4899c920e849a2635bb39492d208
Snow Li <snow_li@brown.edu>
2011-12-11 23:40:53 -0500

    added Constants header

---
+++ scene/Constants.h
@@ -0,0 +1,16 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// HeightMap Gaussian Distr. Constants
+#define HILLS 50.0
+#define RADII 1000.0
+#define SIGMA2X 50.0
+#define SIGMA2Y 50.0
+#define A 4.0
+
+// glwidgetConstants
+#define TERRAIN_HEIGHT 150
+#define TERRAIN_WIDTH 150
+
+
+#endif // CONSTANTS_H
